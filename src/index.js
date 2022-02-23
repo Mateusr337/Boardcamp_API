c5f4d64a46d9b7dde063571cbe2b1c45e6bf50eb
Mateus Cruz <m.cruz.rossetto@gmail.com>
2022-02-23 13:29:24 -0300

    initialization

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import express, { json } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const server = express();
+server.use(cors());
+server.use(json());
+
+server.listen(process.env.PORT, () => {
+    console.log(`listen to port ${process.env.PORT}`);
+});
