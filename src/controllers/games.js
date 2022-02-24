58aba89bdf548b0a67354c2285b98f939bc5adac
Mateus Cruz <m.cruz.rossetto@gmail.com>
2022-02-24 15:30:35 -0300

    feat: get games

--- src/controllers/games.js
+++ src/controllers/games.js
@@ -1,7 +1,6 @@
 import connection from '../db.js';
 
 export async function postGame(req, res) {
-    console.log(req.body);
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
 
     try {
@@ -20,13 +19,27 @@ export async function postGame(req, res) {
 }
 
 export async function getGames(req, res) {
-    const games = (await connection.query(`
+    let sendGames = [];
+
+    try {
+        const games = (await connection.query(`
         SELECT * FROM games;
     `)).rows;
 
-    games.map(game => {
-        console.log(game.categoryId);
-    });
+        for (const game of games) {
+            const { categoryId } = game;
 
-    res.send(games);
+            let categoryName = await connection.query(`
+            SELECT name FROM categories WHERE id = $1;
+        `, [categoryId])
+
+            categoryName = categoryName.rows[0].name;
+            sendGames.push({ ...game, categoryName });
+        }
+    } catch (error) {
+        console.log(error.message);
+        return res.sendStatus(500);
+    }
+
+    res.send(sendGames);
 }
