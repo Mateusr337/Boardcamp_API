1c3395f1d983c8e6c7a332174e92cb5cf957a8a3
Liaess <yann.melo@gmail.com>
2022-03-10 11:12:27 -0300

    Preparando revisão de código

--- src/routes/categoriesRouter.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getCategories, postCategories } from '../controllers/categories.js';
-
-
-const categoriesRouter = Router();
-categoriesRouter.post('/categories', postCategories);
-categoriesRouter.get('/categories', getCategories);
-
-export default categoriesRouter;
--- src/schemas/rentalSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from 'joi';
-
-const rentalSchema = joi.object({
-    customerId: joi.number().integer().required(),
-    gameId: joi.number().integer().required(),
-    daysRented: joi.number().integer().required().min(1)
-});
-
-export default rentalSchema;
